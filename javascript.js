7c9401f9c97c98865c05e1af9ab0acfef4c40392
kvdcre8r <kvdcre8r@gmail.com>
2023-12-07 04:24:12 -0600

    converted form confirmation from page to dialog

--- javascript.js
+++ javascript.js
@@ -1,1 +1,21 @@
-console.log("Hello World");
+document.querySelector("#show-alert").addEventListener("click", async () => {
+    alertDialog();
+})
+
+async function alertDialog() {
+    const dialog = document.createElement("dialog");
+    dialog.innerHTML = `
+    <div id=dialog-content>
+            Your submission has been received! Thank you!
+            <button>OK</button>
+    </div>
+    `;
+    const button = dialog.querySelector("button")
+    return new Promise((res) => {
+        document.body.appendChild(dialog);
+        dialog.showModal();
+        button.addEventListener("click", () => {
+            res(dialog.close())
+        });
+    })
+}
