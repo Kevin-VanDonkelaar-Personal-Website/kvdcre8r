cf41ff6003f0fa4d3607f2d74c518394dc9bc169
kvdcre8r <kvdcre8r@gmail.com>
2023-12-07 22:05:53 -0600

    squashed more bugs

--- javascript.js
+++ javascript.js
@@ -6,8 +6,8 @@ async function alertDialog() {
     const dialog = document.createElement("dialog");
     dialog.innerHTML = `
     <div id=dialog-content>
-            Your submission has been received! Thank you!
-            <button>OK</button>
+        Your submission has been received! Thank you!
+        <button>OK</button>
     </div>
     `;
     const button = dialog.querySelector("button")
