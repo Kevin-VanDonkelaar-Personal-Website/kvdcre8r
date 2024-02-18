99dff2fa8789f53cf4b351886c5b1a2c1ef1c586
kvdcre8r <kvdcre8r@gmail.com>
2024-02-18 08:18:58 -0600

    lots of design and UI changes, need to apply changes to last two sections

--- javascript.js
+++ javascript.js
@@ -1,12 +1,34 @@
 document.querySelector("#show-alert").addEventListener("click", async () => {
-    alertDialog();
+    confirmationDialog();
 })
 
-async function alertDialog() {
+async function confirmationDialog() {
     const dialog = document.createElement("dialog");
     dialog.innerHTML = `
     <div id=dialog-content>
         Your submission has been received! Thank you!
+        <button>OK</button>
+    </div>
+    `;
+    const button = dialog.querySelector("button")
+    return new Promise((res) => {
+        document.body.appendChild(dialog);
+        dialog.showModal();
+        button.addEventListener("click", () => {
+            res(dialog.close())
+        });
+    })
+}
+
+document.querySelector("#p4-more-btn").addEventListener("click", async () => {
+    p4Dialog();
+})
+
+async function p4Dialog() {
+    const dialog = document.createElement("dialog");
+    dialog.innerHTML = `
+    <div id=dialog-content>
+        pws image
         <button>OK</button>
     </div>
     `;
