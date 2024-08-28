30f5e4a6a90c56ccd6f65131f1701cdd3f877a23
kvdcre8r <kvdcre8r@gmail.com>
2024-08-27 20:18:16 -0500

    removed incomplete form feature, squashed bugs, resolved responsive behavior issues

--- javascript.js
+++ javascript.js
@@ -1,43 +1,45 @@
-document.querySelector("#show-alert").addEventListener("click", async () => {
-    confirmationDialog();
-})
+//  *** WORK-IN-PROGRESS ***
 
-async function confirmationDialog() {
-    const dialog = document.createElement("dialog");
-    dialog.innerHTML = `
-    <div id=dialog-content>
-        Your submission has been received! Thank you!
-        <button>OK</button>
-    </div>
-    `;
-    const button = dialog.querySelector("button")
-    return new Promise((res) => {
-        document.body.appendChild(dialog);
-        dialog.showModal();
-        button.addEventListener("click", () => {
-            res(dialog.close())
-        });
-    })
-}
+// document.querySelector("#show-alert").addEventListener("click", async () => {
+//     confirmationDialog();
+// })
 
-document.querySelector("#p4-more-btn").addEventListener("click", async () => {
-    p4Dialog();
-})
+// async function confirmationDialog() {
+//     const dialog = document.createElement("dialog");
+//     dialog.innerHTML = `
+//     <div id=dialog-content>
+//         Your submission has been received! Thank you!
+//         <button>OK</button>
+//     </div>
+//     `;
+//     const button = dialog.querySelector("button")
+//     return new Promise((res) => {
+//         document.body.appendChild(dialog);
+//         dialog.showModal();
+//         button.addEventListener("click", () => {
+//             res(dialog.close())
+//         });
+//     })
+// }
 
-async function p4Dialog() {
-    const dialog = document.createElement("dialog");
-    dialog.innerHTML = `
-    <div id=dialog-content>
-        pws image
-        <button>OK</button>
-    </div>
-    `;
-    const button = dialog.querySelector("button")
-    return new Promise((res) => {
-        document.body.appendChild(dialog);
-        dialog.showModal();
-        button.addEventListener("click", () => {
-            res(dialog.close())
-        });
-    })
-}
+// document.querySelector("#p4-more-btn").addEventListener("click", async () => {
+//     p4Dialog();
+// })
+
+// async function p4Dialog() {
+//     const dialog = document.createElement("dialog");
+//     dialog.innerHTML = `
+//     <div id=dialog-content>
+//         pws image
+//         <button>OK</button>
+//     </div>
+//     `;
+//     const button = dialog.querySelector("button")
+//     return new Promise((res) => {
+//         document.body.appendChild(dialog);
+//         dialog.showModal();
+//         button.addEventListener("click", () => {
+//             res(dialog.close())
+//         });
+//     })
+// }
